d253934b88b2b31f5bd37aed2d3eed5a8f3894aa
parinyad123 <sci.equ@gmail.com>
2020-12-03 15:29:10 +0700

    oop

---
+++ 24_classes_objects.rb
@@ -0,0 +1,17 @@
+class Book
+    attr_accessor :title, :author, :pages
+end
+
+book1 = Book.new()
+book1.title = "Harry Potter"
+book1.author = "JK Rowling"
+book1.pages = 400
+
+book2 = Book.new()
+book2.title = "Lord of the rings"
+book2.author = "Tolkein"
+book2.pages = 500
+
+puts book1.title
+puts book1.pages
+puts book2.author
